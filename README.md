d9522531a9135df9634d10d9403fd4e632b38dc6
Tyler Brooks <thetylerbrooks@gmail.com>
2022-03-07 21:58:48 -0700

    final push, added README, everything working

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# password-generator
+# PASSWORD GENERATOR
+
+## An in-browser password generator that creates a password for the user based on answers to prompts() and confirms()
+
+<br>
+
+[CHECK IT OUT HERE!](https://tbro4.github.io/password-generator/)
+
+<br>
+
+![password generator](./assets/images/passgen.png)
+
+<br>
+
+## When the Generate Password button is clicked, the user is asked to input a length of 8 to 128
+
+<br>
+
+![enter length](./assets/images/EnterLength.png)
+
+## Invalid options tell the user to enter a valid option and restarts the process
+
+<br>
+
+![enter valid number](./assets/images/EnterValidNum.png)
+
+## The user is then asked to confirm at least one variable type (via 4 seperate confirms()) (lowercase, uppercase, numbers, and/or special characters)
+
+<br>
+
+![include lowercase?](./assets/images/Lowercase.png)
+
+<br>
+
+![include uppercase?](./assets/images/Uppercase.png)
+
+<br>
+
+![include numbers?](./assets/images/Numbers%3F.png)
+
+<br>
+
+![include special characters?](./assets/images/SpecChar%3F.png)
+
+<br>
+
+## If they hit no (Cancel) on all four prompts they are asked to confirm at least one type
+
+<br>
+
+![confirm at least one type](./assets/images/Atleastone.png)
+
+<br>
+
+## A password is displayed on the page that fits the chosen criteria
+
+<br>
+
+![Password displayed](./assets/images/result.png)
--- assets/script.js
+++ assets/script.js
@@ -93,13 +93,88 @@ var specChar = [
   "?",
 ];
 
+var userPossibleChar = [];
+
+var finalPass = [];
+
 var generateBtn = document.querySelector("#generate");
+
+function gatherInfo() {
+  var passLength = prompt(
+    "Please enter a password length of 8 to 128 characters"
+  );
+  // console.log(passLength);
+  //if length is valid, move into other prompts
+  if (passLength >= 8 && passLength <= 128) {
+    function charTypes() {
+      var includeLowercase = confirm(
+        "You must choose at least one character type (lowercase, uppercase, numbers, special characters): \n\nWould you like to include lowercase letters?"
+      );
+
+      var includeUppercase = confirm(
+        "Would you like to include uppercase letters?"
+      );
+      var includeNum = confirm("Would you like to include numbers?");
+      var includeSpecChar = confirm(
+        "Would you like to include special characters?"
+      );
+
+      // after prompts are answered, if true, pushes array values to new array
+      if (includeLowercase) {
+        userPossibleChar = userPossibleChar.concat(allLower);
+      }
+      if (includeUppercase) {
+        userPossibleChar = userPossibleChar.concat(allCaps);
+      }
+      if (includeNum) {
+        userPossibleChar = userPossibleChar.concat(numbers);
+      }
+      if (includeSpecChar) {
+        userPossibleChar = userPossibleChar.concat(specChar);
+      }
+      //if none are selected, takes us back to char type prompts
+      if (
+        !includeLowercase &&
+        !includeUppercase &&
+        !includeNum &&
+        !includeSpecChar
+      ) {
+        alert("Please confirm at least one character type");
+        charTypes();
+      }
+    }
+    //calls function for the first time
+    charTypes();
+  } else {
+    alert("Please enter a valid number");
+    gatherInfo();
+  }
+  //Math.floor and .random to create string of characters from user-modified array
+  for (var i = 0; i < passLength; i++) {
+    var character =
+      userPossibleChar[Math.floor(Math.random() * userPossibleChar.length)];
+    finalPass.push(character);
+  }
+  // console.log(finalPass.join(""));
+}
 
 // Write password to the #password input
 function writePassword() {
+  //varibale that calls a function?
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  function generatePassword() {
+    //resets arrays to empty if you want to create another password
+    finalPass = [];
+    userPossibleChar = [];
+    //fills array with characters
+    gatherInfo();
+    // console.log(finalPass.join(""));
+    //returns all characters in finalPass array with no spaces
+    return finalPass.join("");
+  }
 
+  // value is assigned through the generatePassword function
   passwordText.value = password;
 }
 
